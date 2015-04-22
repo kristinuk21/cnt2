555d6e3586b19fefaf73c9e73ee42bc33244f2e3
kristinuk21 <kristinuk21@gmail.com>
2015-04-23 00:02:50 +0300

    Update project.js

--- project.js
+++ project.js
@@ -7,22 +7,22 @@ angular.module('cntApp', [])
     cnt.compute = function() {
        //var current_date = new Date().getDate();
        
-       if (text:cnt.end == ''){
-         cnt.end = 20;
-       } else {
+       if (text:cnt.end){
          cnt.end = text:cnt.end;
+       } else {
+         cnt.end = 20;
        }
        
-       if (text:cnt.start == ''){
-         cnt.start = 2000;
-       } else {
+       if (text:cnt.start){
          cnt.start = text:cnt.start;
+       } else {
+         cnt.start = 2000;
        }
        
-      if (text:cnt.rate == ''){
-         cnt.rate = -2000/31;
-       } else {
+      if (text:cnt.rate){
          cnt.rate = text:cnt.rate;
+       } else {
+         cnt.rate = -2000/31;
        }
       
       //var remain = Math.abs(cnt.end - current_date)*cnt.rate + cnt.start;
