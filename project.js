b45316e1774560db5ac0d4f6eeaa24b1ee1835c5
kristinuk21 <kristinuk21@gmail.com>
2015-04-22 23:13:20 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,103 +1,28 @@
-angular.module('project', ['ngRoute', 'firebase'])
+angular.module('todoApp', [])
+  .controller('TodoListController', function() {
+    var todoList = this;
+    todoList.todos = [
+      {text:'learn angular', done:true},
+      {text:'build an angular app', done:false}];
  
-.value('fbURL', 'https://ng-projects-list.firebaseio.com/')
-.service('fbRef', function(fbURL) {
-  return new Firebase(fbURL)
-})
-.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
-  var auth;
-  return function () {
-      if (auth) return $q.when(auth);
-      var authObj = $firebaseAuth(fbRef);
-      if (authObj.$getAuth()) {
-        return $q.when(auth = authObj.$getAuth());
-      }
-      var deferred = $q.defer();
-      authObj.$authAnonymously().then(function(authData) {
-          auth = authData;
-          deferred.resolve(authData);
-      });
-      return deferred.promise;
-  }
-})
- 
-.service('Projects', function($q, $firebase, fbRef, fbAuth) {
-  var self = this;
-  this.fetch = function () {
-    if (this.projects) return $q.when(this.projects);
-    return fbAuth().then(function(auth) {
-      var deferred = $q.defer();
-      var ref = fbRef.child('projects/' + auth.auth.uid);
-      var $projects = $firebase(ref);
-      ref.on('value', function(snapshot) {
-        if (snapshot.val() === null) {
-          $projects.$set(window.projectsArray);
-        }
-        self.projects = $projects.$asArray();
-        deferred.resolve(self.projects);
-      });
-      return deferred.promise;
-    });
-  };
-})
- 
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller:'ProjectListController as projectList',
-      templateUrl:'list.html',
-      resolve: {
-        projects: function (Projects) {
-          return Projects.fetch();
-        }
-      }
-    })
-    .when('/edit/:projectId', {
-      controller:'EditProjectController as editProject',
-      templateUrl:'detail.html'
-    })
-    .when('/new', {
-      controller:'NewProjectController as editProject',
-      templateUrl:'detail.html'
-    })
-    .otherwise({
-      redirectTo:'/'
-    });
-})
- 
-.controller('ProjectListController', function(projects) {
-  var projectList = this;
-  projectList.projects = projects;
-})
- 
-.controller('NewProjectController', function($location, Projects) {
-  var editProject = this;
-  editProject.save = function() {
-      Projects.$add(editProject.project).then(function(data) {
-          $location.path('/');
-      });
-  };
-})
- 
-.controller('EditProjectController',
-  function($location, $routeParams, Projects) {
-    var editProject = this;
-    var projectId = $routeParams.projectId,
-        projectIndex;
- 
-    editProject.projects = Projects.projects;
-    projectIndex = editProject.projects.$indexFor(projectId);
-    editProject.project = editProject.projects[projectIndex];
- 
-    editProject.destroy = function() {
-        editProject.projects.$remove(editProject.project).then(function(data) {
-            $location.path('/');
-        });
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
     };
  
-    editProject.save = function() {
-        editProject.projects.$save(editProject.project).then(function(data) {
-           $location.path('/');
-        });
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
     };
-});
+ 
+    todoList.archive = function() {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  });
