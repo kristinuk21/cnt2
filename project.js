47843b7dff848d22f3d23bd69424afdb644ce862
kristinuk21 <kristinuk21@gmail.com>
2015-05-08 01:46:11 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,46 +1,53 @@
 angular.module('cntApp', ['ui.bootstrap']).controller('ProgressBar', function ($scope) {
-  $scope.max = 100;
   $scope.compute = function() {
-  var date1 = new Date();
-  var date2 = new Date(); //next restart
-  date2.setDate(20); //end_date
-  date2.setHours(18); //end_date hours
-  date2.setHours(18,00); //end_date hours and minutes
-  
-  
-  if (date1.getDate()  > 20){
-    // move date2 to next month
-    if (date2.getMonth() < 11) {
-      date2.setMonth(date2.getMonth()+1);
-    } else {
-      date2.setMonth(0);
-      date2.setYear(date2.getYear()+1);
-    }
-  }
-  
-  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-  $scope.days =   diffDays;
-  $scope.days_precise =   timeDiff / (1000 * 3600 * 24);
-  
-  var date3 = new Date(date2.getTime()); //previos restart
-    if (date2.getMonth() > 1) {
-      date3.setMonth(date3.getMonth()-1);
-    } else {
-      date3.setMonth(11);
-      date3.setYear(date3.getYear()-1);
-    }
-  
-  var t_timeDiff = Math.abs(date3.getTime() - date2.getTime());
-  var t_diffDays = Math.ceil(t_timeDiff / (1000 * 3600 * 24)); 
- 
- $scope.days_p_precise = Math.floor(100*diffDays/(t_timeDiff / (1000 * 3600 * 24))); 
- $scope.days_p = Math.floor(100*diffDays/t_diffDays);
+    
+    var one_day_ms = (1000 * 3600 * 24);
+    var budget = 2000;
+    var budget_rounding = 50;
+    
+    //curent time
+    var now_date = new Date();
+    // next restart
+    var next_date = new Date(); 
 
-  var remain = Math.floor(2000 - ((2000*(t_diffDays - diffDays))/t_diffDays));
-  $scope.amount_precise = remain;
-  $scope.amount = 50*Math.ceil(remain/50);
-  //$scope.amount_p = Math.floor(100*remain/2000);
+    next_date.setDate(20); //end_date
+    next_date.setHours(18,00); //end_date hours and minutes
+    
+    // check and increment month
+    if (now_date.getTime() < next_date.getTime()){
+      // move next_date to next month
+      if (next_date.getMonth() < 11) {
+        next_date.setMonth(next_date.getMonth()+1);
+      } else {
+        next_date.setMonth(0);
+        next_date.setYear(next_date.getYear()+1);
+      }
+    }
+    
+    // prevvious restart
+    var prev_date = new Date(next_date.getTime()); //previos restart
+    
+    // decrement a month
+    if (next_date.getMonth() > 1) {
+      prev_date.setMonth(prev_date.getMonth()-1);
+    } else {
+      prev_date.setMonth(11);
+      prev_date.setYear(prev_date.getYear()-1);
+    }
+    
+    //compute delat dates
+    var remain_time = Math.abs(next_date.getTime() - now_date.getTime());
+    var restart_time = Math.abs(next_date.getTime() - prev_date.getTime());
+    
+    //days percentage - progrss bar values
+    $scope.days_p_precise = Math.floor(100*remain_time/restart_time);
+    $scope.days_p = (one_day_ms) * Math.floor(100*remain_time/(restart_time*one_day_ms));
+    
+    //compute amount
+    var remain = budget - ((restart_time - remain_time)*budget/restart_time);
+    $scope.amount_precise = Math.floor(remiain*100)/100; //two digits
+    $scope.amount = budget_rounding*Math.ceil(remain/budget_rounding); // rounded
+
   };
   $scope.compute();
 
