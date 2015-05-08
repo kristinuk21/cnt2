26f240be267e48a5dc54be1eacf63dc991bd236c
kristinuk21 <kristinuk21@gmail.com>
2015-05-08 17:47:33 +0300

    Update project.js

--- project.js
+++ project.js
@@ -49,7 +49,7 @@ angular.module('cntApp', ['ui.bootstrap']).controller('ProgressBar', function ($
 
     //compute amount
     var remain = budget - ((restart_time - remain_time)*budget/restart_time);
-    $scope.amount_precise = Math.floor(remain*100)/100; //two digits
+    $scope.amount_precise = remain.toFixed(4); //four digits
     $scope.amount = budget_rounding*Math.ceil(remain/budget_rounding); // rounded
     
     // Time untill the above(non precise) will change (%)
